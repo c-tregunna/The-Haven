390264726108b389a0601cc27c58615bcbab6b9f
Claire Tregunna <c.tregunna1980@gmail.com>
2020-11-25 15:53:34 +0000

    create home.html and add form

--- js/app.js
+++ js/app.js
@@ -1,20 +1,17 @@
 const overlayLogo = document.querySelector('.overlay-logo');
 const overlay = document.querySelector('.enter-overlay');
 const enterBtn = document.querySelector('.enter');
-const heading = document.querySelector('.show-list');
-const list = document.querySelector('.list');
+// const heading = document.querySelector('.show-list');
+// const list = document.querySelector('.list');
 const contentSpan = document.querySelectorAll('.section_container span');
 const sectionContent = document.querySelector('.section_content');
 
-//------ Gallery plugin code -----\\
+const sessionHideOverlay = sessionStorage.getItem('enter');
 
-baguetteBox.run('.gallery', {
-    animation: 'fadeIn',
-    noScrollbars: true
-});
 
 
 //------- Hide enter overlay ------\\
+function hideOverlay() {
 if(enterBtn) {
 enterBtn.addEventListener('click', e => {
     overlay.classList.add('hidden');
@@ -22,14 +19,30 @@ enterBtn.addEventListener('click', e => {
     enterBtn.style.display = 'none';
     overlayLogo.style.display = 'none';
     }, 900);
-})
+    setTimeout(() => {
+    location.href = "home.html";
+    },1050);
+    })
+    }
 };
+hideOverlay();
+
+
+//------ Session storage to keep overlay hidden when in the site -----\\
+//I see what you're talking about - I think that you could do it through localStorage... basically write some code into localStorage after the Enter the Haven is clicked and when index.html loads only show the overlay if that localStorage variable is not present?  But that also means the user would only ever see it once ... actually you could "avoid" that by using session storage instead of local storage so they saw it each time
+//Yeah the basic gist in the office hours will help you but you will have to run a conditional that checks if the local storage value is set and hides the overlay
+sessionStorage.setItem('occupation', 'Web Developer');
+
+console.log(sessionStorage.getItem('occupation'));
+
 
 //------ Copywrite year -----\\
 let today = new Date();
 let year = today.getFullYear();
 let currentYear = document.querySelector('#date');
 currentYear.innerHTML = year;
+
+
 
 
 
